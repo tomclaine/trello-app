fa1bca7030910bec18ae55b794268bfde2e745d1
tomclaine <leduykhanh219@gmail.com>
2023-07-12 15:26:31 +0700

    design_ui

--- src/App.js
+++ src/App.js
@@ -1,9 +1,135 @@
-import './App.css';
+import './App.scss';
 
 function App() {
   return (
-    <div className="App">
-      <h1 className='hello'>Hello</h1>
+    <div className="trello-app">
+      <header>
+        <div className='logo'>TrelloApp</div>
+      </header>
+      <nav className='navbar'>navbar</nav>
+      <div className='board-columns'>
+        <div className='column'>
+          <div className='header'>
+            Title
+          </div>
+          <ul>
+            <li>todo1</li>
+            <li>todo2</li>
+            <li>todo3</li>
+            <li>todo4</li>
+            <li>todo5</li>
+            <li>todo1</li>
+            <li>todo2</li>
+            <li>todo3</li>
+            <li>todo4</li>
+            <li>todo5</li>
+            <li>todo1</li>
+            <li>todo2</li>
+            <li>todo3</li>
+            <li>todo4</li>
+            <li>todo5</li>
+          </ul>
+          <div className='footer'>
+            Add new todo
+          </div>
+        </div> 
+
+        <div className='column'>
+          <div className='header'>
+            Title
+          </div>
+          <ul>
+            <li>todo1</li>
+            <li>todo2</li>
+            <li>todo3</li>
+            <li>todo4</li>
+            <li>todo5</li>
+          </ul>
+          <div className='footer'>
+            Add new todo
+          </div>
+        </div> 
+
+        <div className='column'>
+          <div className='header'>
+            Title
+          </div>
+          <ul>
+            <li>todo1</li>
+            <li>todo2</li>
+            <li>todo3</li>
+            <li>todo4</li>
+            <li>todo5</li>
+          </ul>
+          <div className='footer'>
+            Add new todo
+          </div>
+        </div>
+
+        <div className='column'>
+          <div className='header'>
+            Title
+          </div>
+          <ul>
+            <li>todo1</li>
+            <li>todo2</li>
+            <li>todo3</li>
+            <li>todo4</li>
+            <li>todo5</li>
+          </ul>
+          <div className='footer'>
+            Add new todo
+          </div>
+        </div>
+
+        <div className='column'>
+          <div className='header'>
+            Title
+          </div>
+          <ul>
+            <li>todo1</li>
+            <li>todo2</li>
+            <li>todo3</li>
+            <li>todo4</li>
+            <li>todo5</li>
+          </ul>
+          <div className='footer'>
+            Add new todo
+          </div>
+        </div>
+
+        <div className='column'>
+          <div className='header'>
+            Title
+          </div>
+          <ul>
+            <li>todo1</li>
+            <li>todo2</li>
+            <li>todo3</li>
+            <li>todo4</li>
+            <li>todo5</li>
+          </ul>
+          <div className='footer'>
+            Add new todo
+          </div>
+        </div>
+
+        <div className='column'>
+          <div className='header'>
+            Title
+          </div>
+          <ul>
+            <li>todo1</li>
+            <li>todo2</li>
+            <li>todo3</li>
+            <li>todo4</li>
+            <li>todo5</li>
+          </ul>
+          <div className='footer'>
+            Add new todo
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
