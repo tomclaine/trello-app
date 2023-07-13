5b6aa21b6259277d27c0a45a44cc0c113646a791
tomclaine <leduykhanh219@gmail.com>
2023-07-13 10:35:43 +0700

    Initial sample data

--- src/components/Column/Column.js
+++ src/components/Column/Column.js
@@ -1,36 +1,18 @@
 import React from "react"
 import './Column.scss'
-import Task from "components/Task/Task"
+import { mapOrder } from "utilities/sorts"
+import Card from "components/Card/Card"
 
-function Column(){
+function Column(props){
+    const {column} = props 
+    const cards = mapOrder(column.cards, column.cardOrder, 'id')
     return( 
         <div className='column'>
             <div className='header'>
-                Title
+                {column.title}
             </div>
-            <ul className="task-list">
-                <Task />
-                <Task />
-                <Task />
-                <Task />
-                <Task />
-                <Task />
-                <Task />
-                <Task />
-                {/* <li className="task-item">todo2</li>
-                <li className="task-item">todo3</li>
-                <li className="task-item">todo4</li>
-                <li className="task-item">todo5</li>
-                <li className="task-item">todo1</li>
-                <li className="task-item">todo2</li>
-                <li className="task-item">todo3</li>
-                <li className="task-item">todo4</li>
-                <li className="task-item">todo5</li>
-                <li className="task-item">todo1</li>
-                <li className="task-item">todo2</li>
-                <li className="task-item">todo3</li>
-                <li className="task-item">todo4</li>
-                <li className="task-item">todo5</li> */}
+            <ul className="card-list">
+                {cards.map((card, index) => <Card key={index} card={card} />)}
             </ul>
             <div className='footer'>
                 Add new todo
